ad6346467b3fb31daa85575fa562f1d01485e337
Yuvraj Bains <yuvrajbains112@gmail.com>
2025-05-05 15:19:15 -0400

    last commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://personal-task-manager-production.up.railway.app/api', 
+  baseURL: import.meta.env.RAILWAY_API_URL, 
 });
 
 API.interceptors.request.use((config) => {
